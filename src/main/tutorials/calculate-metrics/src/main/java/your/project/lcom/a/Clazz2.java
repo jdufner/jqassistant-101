56de60c14284c03060154d7d05354c1f5e877019
Jürgen Dufner <juergen@jdufner.de>
2019-03-09 00:16:29 +0100

    added lcom4 metrics

---
+++ src/main/tutorials/calculate-metrics/src/main/java/your/project/lcom/a/Clazz2.java
@@ -0,0 +1,23 @@
+package your.project.lcom.a;
+
+final class Clazz2 {
+
+  private int value = 1;
+
+  int getValue() {
+    return value;
+  }
+
+  int getIncrementedValue() {
+    return getValue() + 1;
+  }
+
+  int getDoubleIncrementedValue() {
+    return getIncrementedValue() + 1;
+  }
+
+  int getTripleIncrementedValue() {
+    return getDoubleIncrementedValue() + 1;
+  }
+
+}
