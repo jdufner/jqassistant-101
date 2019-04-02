d85ab435cb2139bad6ffaa6abe578489e4a9123b
Jürgen Dufner <juergen@jdufner.de>
2019-04-02 23:29:58 +0200

    added test for lcom4 metric

---
+++ src/main/tutorials/calculate-metrics/src/main/java/your/project/lcom/a/Clazz3.java
@@ -0,0 +1,21 @@
+package your.project.lcom.a;
+
+final class Clazz3 {
+
+  private int field1;
+  private int field2;
+  private int field3;
+
+  int method1() {
+    return  field1;
+  }
+
+  int method2() {
+    return field2 + field3;
+  }
+
+  int method3() {
+    return field3;
+  }
+
+}
