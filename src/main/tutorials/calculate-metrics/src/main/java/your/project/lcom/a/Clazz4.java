edc7da56915070f0f848bfbeab6a825cfe0431e9
Jürgen Dufner <juergen@jdufner.de>
2019-04-07 09:03:33 +0200

    renamed to cohesion and added tests

---
+++ src/main/tutorials/calculate-metrics/src/main/java/your/project/lcom/a/Clazz4.java
@@ -0,0 +1,35 @@
+package your.project.lcom.a;
+
+final class Clazz4 {
+
+  private int field1;
+
+  Clazz4(int field1) {
+    this.field1 = field1;
+  }
+
+  int getField1() {
+    return field1;
+  }
+
+  double getSqrtOfField1() {
+    return Math.sqrt((double) field1);
+  }
+
+  double getLogOfField1() {
+    return Math.log((double) field1);
+  }
+
+  double getCosOfField1() {
+    return Math.cos((double) field1);
+  }
+
+  double getSinOfField1() {
+    return Math.sin((double) field1);
+  }
+
+  double getTanOfField1() {
+    return Math.tan((double) field1);
+  }
+
+}
